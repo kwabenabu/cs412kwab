dff02dc4b0580ef111a8a87a6229626218a4a543
Kwabena Ampomah <kwabamp@bu.edu>
2025-12-08 17:45:15 -0500

    idk

---
+++ my-cards-app/app/(tabs)/add_joke.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function AddJokeScreen() {
+	return (
+		<>
+			{/* TODO: Add joke form UI here */}
+			<div>Add Joke Screen</div>
+		</>
+	);
+}
---
+++ my-cards-app/app/(tabs)/jokes_list.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function JokesListScreen() {
+	return (
+		<>
+			{/* TODO: Add jokes list UI here */}
+			<div>Jokes List Screen</div>
+		</>
+	);
+}
+
+// ...existing code...
