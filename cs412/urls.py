ba2bdbd67dd7e49f2cd1f3de5af7536796ba601e
Kwabena Ampomah <kwabamp@bu.edu>
2025-12-09 11:36:10 -0500

    why

--- cs412/urls.py
+++ cs412/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -28,6 +29,9 @@ urlpatterns = [
     path('mini_insta/', include('mini_insta.urls')),
     path('voter_analytics/', include('voter_analytics.urls')),
     path('project/', include('project.urls')),  # Soccer trading card portfolio
+
+    # Redirect /login/ to /project/login/
+    path('login/', lambda request: redirect('project:login', permanent=False)),
 
 ]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
